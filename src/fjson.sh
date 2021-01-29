86a0e9037291f67bb22133cb835d047ea561683d
Zane Rockenbaugh <zane@liquid-labs.com>
2021-01-29 11:52:37 -0600

    added simple command line script to read a file and echo it out [no ci]

---
+++ src/fjson.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#!/usr/bin/env bash
+
+MY_DIR="$(dirname "${0}")" # this will be the package dist dir
+NODE="/usr/bin/env node"
+
+${NODE} -e "const fjson = require('.'); console.log(JSON.stringify(fjson.read('${1}'), null, 2))"
